6f0c5833d432d47a726ffe6a04925f408e216e4e
https://github.com/turmeque <jhojangutierrez900@gmail.com>
2022-10-12 17:27:40 -0500

    holaaaaaaaaaaa

--- config/mail.js
+++ config/mail.js
@@ -1,15 +1,14 @@
-// require('dotenv').config();
-// const nodemailer = require("nodemailer");
-// const { email, adminApi } = process.env
+require('dotenv').config();
+const nodemailer = require("nodemailer");
+const { email, adminApi } = process.env
 
-// export const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 465,
-//   secure: true, // true for 465, false for other ports
-//   auth: {
-//     user: email,
-//     pass: adminApi,
-//   },
-// });
+export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: email,
+    pass: adminApi,
+  },
+});
 
-///???''''''?????'
