b59686c019b503e84a1cc1c86be39062fc27c6d1
https://github.com/turmeque <jhojangutierrez900@gmail.com>
2022-10-12 22:13:06 -0500

    hola

--- config/config.js
+++ config/config.js
@@ -6,26 +6,12 @@ module.exports = {
     
     
     password: process.env.DB_PASSWORD || "JOHAN",
-    database: process.env.DB_NAME || "animes",
-    host: process.env.DB_HOST || "localhost",
+   // database: process.env.DB_NAME || "animes",
+    //host: process.env.DB_HOST || "localhost",
     username: process.env.DB_USER || "postgres",
-    "port": 5432,
+  
     dialect: "postgres",
     logging: false,
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "postgres",
-  },
-  production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    
-    host: "127.0.0.1",
-    dialect: "postgres",
-  },
+  }
+
 };
