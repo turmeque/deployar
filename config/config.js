294695cb852534e2647edbf60371d83d2ffe5bb6
https://github.com/turmeque <jhojangutierrez900@gmail.com>
2022-10-12 21:47:56 -0500

    holaaaaaaaaaaaaaaaaaagit status11!

--- config/config.js
+++ config/config.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 
 module.exports = {
   development: {
+    
+    
+    
     password: process.env.DB_PASSWORD || "JOHAN",
     database: process.env.DB_NAME || "animes",
     host: process.env.DB_HOST || "localhost",
     username: process.env.DB_USER || "postgres",
- 
+    "port": 5432,
     dialect: "postgres",
     logging: false,
   },
