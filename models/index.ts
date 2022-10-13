5017777396921dc39867a5ecbe79265be85fd91c
https://github.com/turmeque <jhojangutierrez900@gmail.com>
2022-10-12 22:59:16 -0500

    deployer222

--- models/index.ts
+++ models/index.ts
@@ -1,41 +1,28 @@
-"use strict";
-require("dotenv").config();
-const fs = require("fs");
-const path = require("path");
-const Sequelize = require("sequelize");
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.js")[env];
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname)[env];
 const db: any = {};
 
- let sequelize: any;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+let sequelize: any;
+sequelize = new Sequelize(`postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`)
 
-fs.readdirSync(__dirname)
+fs
+  .readdirSync(__dirname)
   .filter((file: string) => {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".ts"
-    );
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.ts');
   })
   .forEach((file: any) => {
     // console.log(file)
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach((modelName) => {
+Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
